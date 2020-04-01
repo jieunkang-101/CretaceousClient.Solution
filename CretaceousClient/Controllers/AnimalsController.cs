91c26e8aafedea6b75656d7319e8ccaf57fda56f
Jieun Kang <jieunkang101@gmail.com>
2020-04-01 09:14:18 -0700

    add initial files and add Animal class an ApiHelper.cs

---
+++ CretaceousClient/Controllers/AnimalsController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CretaceousClient.Models;
+
+namespace CretaceousClient.Controllers
+{
+  public class AnimalsController : Controller
+  {
+    public IActionResult Index()
+    {
+      var allAnimals = Animal.GetAnimals();
+      return View(allAnimals);
+    }
+  }
+}            
+        
