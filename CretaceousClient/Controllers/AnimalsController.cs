64a30cf92d2dfa095b5c3ee11c24bbee8fe0690d
Jieun Kang <jieunkang101@gmail.com>
2020-04-01 10:07:56 -0700

    add CRUD functionality

--- CretaceousClient/Controllers/AnimalsController.cs
+++ CretaceousClient/Controllers/AnimalsController.cs
@@ -16,10 +16,38 @@ namespace CretaceousClient.Controllers
       return View(allAnimals);
     }
 
+    [HttpPost]
+    public IActionResult Index(Animal animal)
+    {
+      Animal.Post(animal);
+      return RedirectToAction("Index");
+    }
+
     public IActionResult Details(int id)
     {
       var animal = Animal.GetDetails(id);
       return View(animal);
+    }
+
+    public IActionResult Edit(int id)
+    {
+      var animal = Animal.GetDetails(id);
+      return View(animal);
+    }
+
+    [HttpPost]
+    public IActionResult Details(int id, Animal animal)
+    {
+      animal.AnimalId = id;
+      Animal.Put(animal);
+      return RedirectToAction("Details", id);
+    }
+
+    [HttpPost]
+    public IActionResult Delete(int id)
+    {
+      Animal.Delete(id);
+      return RedirectToAction("Index");
     }
   }
 }            
